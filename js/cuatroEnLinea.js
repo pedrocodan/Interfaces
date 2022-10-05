13c13df787bd23e749efd1e3eb498e84b9fbcb11
Pedro Codan <codanpedro@gmail.com>
2022-10-05 15:10:47 -0300

    ensamble

---
+++ js/cuatroEnLinea.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var compartir = document.querySelector("#compartir");
+var redes = document.querySelector("#redes");
+compartir.addEventListener("click", () => {
+    redes.classList.toggle("redes")});
+
+
+
--- js/index.js
+++ js/index.js
@@ -8,3 +8,39 @@ document.getElementById('btn-profile').addEventListener('click', () => {
     document.getElementById('nav-profile').classList.toggle('show-profile');
 });
 
+
+var i = 0;
+window.onload = move();
+
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("myBar");
+        var contenedor = document.getElementById("myProgress")
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+                document.getElementById("myProgress").classList.add("borrar");
+                document.getElementById("paginaCargandoDeFondo").classList.remove("borrar");
+            } else {
+
+                width++;
+                var porcen = document.createElement("p");
+                var text = document.createTextNode(width + "%");
+                porcen.appendChild(text);
+                elem.appendChild(porcen);
+                elem.style.width = width + "%";
+                porcen.setAttribute("id", "porcentaje"+ (width));
+                document.getElementById("porcentaje" + (width-1)).classList.add("borrar");
+                document.getElementById("paginaCargandoDeFondo").classList.add("borrar");
+            }
+        } 
+    }
+}
+var btnScrollTop = document.querySelector("#scrollToTop");
+btnScrollTop.addEventListener("click", () => {
+    window.scrollTo(0,0);
+})
