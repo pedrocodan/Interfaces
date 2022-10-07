faa174d56e944b4d83d6e0703aab053336e46233
ana <anacelcan@gmail.com>
2022-10-07 20:48:57 -0300

    Se arregla bug en flecha der y refactor de js

--- Entregable2/js/index.js
+++ Entregable2/js/index.js
@@ -49,213 +49,121 @@ btnScrollTop.addEventListener("click", () => {
 
 /*CARRUSELES */// Adaptado para un id 1
 
-const fila = document.getElementById('contenedor-carrusel-1');
-const juegos = document.getElementById('carrusel-1').querySelectorAll('.juego');
+function generarFlecha(id) {
+    var flechaizquierda = document.getElementById('flecha-izquierda-' + id);
+    var flechaderecha = document.getElementById('flecha-derecha-' + id);
+    var fila = document.getElementById('contenedor-carrusel-' + id);
+    var indicadores = document.getElementById('indicadores-' + id);
+    var juegos = document.getElementById('carrusel-' + id).querySelectorAll('.juego');
 
-const flechaizquierda = document.getElementById('flecha-izquierda-1');
-const flechaderecha = document.getElementById('flecha-derecha-1');
-
-flechaizquierda.addEventListener('click', () => {
-    flechaizquierda.classList.add('flecha-oculta');
-
-    fila.scrollLeft -= fila.offsetWidth;
-
-    const indicadoractivo = document.getElementById('indicadores-1').querySelector('.indicadores .activo');
-    console.log(indicadoractivo.previousSibling);
-    if(indicadoractivo.previousSibling) {
-        indicadoractivo.previousSibling.classList.add('activo');
-        indicadoractivo.classList.remove('activo');
-    }
-
-    juegos.forEach(juego => juego.classList.remove('hover'));
-
-    setTimeout(() => {
-        flechaizquierda.classList.remove('flecha-oculta');
-    }, 500);
-});
-
-flechaderecha.addEventListener('click', () => {
-    flechaderecha.classList.add('flecha-oculta');
-
-    fila.scrollLeft += fila.offsetWidth;
-
-    const indicadoractivo = document.getElementById('indicadores-1').querySelector('.indicadores .activo');
-    if(indicadoractivo.nextSibling){
-        indicadoractivo.nextElementSibling.classList.add('activo');
-        indicadoractivo.classList.remove('activo');
-    }
-
-    juegos.forEach(juego => juego.classList.remove('hover'));
- 
-    setTimeout(() => {
-        flechaderecha.classList.remove('flecha-oculta');
-    }, 500);
-});
-
-const numeroPaginas = Math.ceil(juegos.length / 5);
-
-
-
-for (let i = 0; i < numeroPaginas; i++) {
-    const indicator = document.createElement('button');
-
-    if (i == 0) {
-        indicator.classList.add('activo');
-    }
-
-    document.getElementById('indicadores-1').appendChild(indicator);
-
-    indicator.addEventListener('click', (e) => {
-        fila.scrollLeft = i * fila.offsetWidth;
-
-        document.getElementById('indicadores-1').querySelector('.indicadores .activo').classList.remove('activo');
-        e.target.classList.add('activo');
+    flechaizquierda.addEventListener('click', () => {
+        flechaizquierda.classList.add('flecha-oculta');
+    
+        fila.scrollLeft -= fila.offsetWidth;
+    
+        const indicadoractivo = indicadores.querySelector('.indicadores .activo');
+        
+        if(indicadoractivo.previousSibling?.tagName == 'BUTTON') {
+            indicadoractivo.previousSibling.classList.add('activo');
+            indicadoractivo.classList.remove('activo');
+        }
+    
+        juegos.forEach(juego => juego.classList.remove('hover'));
+    
+        setTimeout(() => {
+            flechaizquierda.classList.remove('flecha-oculta');
+        }, 500);
+    });
+    
+    flechaderecha.addEventListener('click', () => {
+        flechaderecha.classList.add('flecha-oculta');
+    
+        fila.scrollLeft += fila.offsetWidth;
+    
+        const indicadoractivo = indicadores.querySelector('.indicadores .activo');
+        if(indicadoractivo.nextSibling){
+            indicadoractivo.nextElementSibling.classList.add('activo');
+            indicadoractivo.classList.remove('activo');
+        }
+    
+        juegos.forEach(juego => juego.classList.remove('hover'));
+     
+        setTimeout(() => {
+            flechaderecha.classList.remove('flecha-oculta');
+        }, 500);
     });
 }
 
-juegos.forEach((juego) => {
-    juego.addEventListener('mouseenter', (e) => {
-        const elemento = e.currentTarget;
-        setTimeout(() => {
-            juegos.forEach(juego => juego.classList.remove('hover'));
-            elemento.classList.add('hover');
-
-            let imagen = elemento.querySelector('img');
-            if(!imagen.getAttribute('src').includes('_hover')) {
-                //Solo para desktop
-                //Reemplazamos la img sin hover por la con hover
-                let urlImgHover = imagen.getAttribute('src').replace('.','_hover.');
-                imagen.setAttribute('src',urlImgHover);
-            }
-        }, 200);
-    });
-
-    juego.addEventListener('mouseleave', (e) => {
-        const elemento = e.currentTarget;
-        setTimeout(() => {
-            elemento.classList.remove('hover');
-            let imagen = elemento.querySelector('img');
-            let urlImg = imagen.getAttribute('src').replaceAll('_hover','');
-            imagen.setAttribute('src', urlImg);
-        }, 200);
-    });
-});
-
-// Adaptado para un id 2
-
-const fila2 = document.getElementById('contenedor-carrusel-2');
-const juegos2 = document.getElementById('carrusel-2').querySelectorAll('.juego');
-
-const flechaizquierda2 = document.getElementById('flecha-izquierda-2');
-const flechaderecha2 = document.getElementById('flecha-derecha-2');
-
-flechaizquierda2.addEventListener('click', () => {
-    flechaizquierda2.classList.add('flecha-oculta');
-
-    fila2.scrollLeft -= fila2.offsetWidth;
-
-    const indicadoractivo = document.getElementById('indicadores-2').querySelector('.indicadores .activo');
-    if(indicadoractivo.previousSibling) {
-        indicadoractivo.previousSibling.classList.add('activo');
-        indicadoractivo.classList.remove('activo');
-    }
-
-    juegos2.forEach(juego => juego.classList.remove('hover'));
-
-    setTimeout(() => {
-        flechaizquierda2.classList.remove('flecha-oculta');
-    }, 500);
-});
-
-flechaderecha2.addEventListener('click', () => {
-    flechaderecha2.classList.add('flecha-oculta');
-
-    fila2.scrollLeft += fila2.offsetWidth;
-
-    const indicadoractivo = document.getElementById('indicadores-2').querySelector('.indicadores .activo');
-    if(indicadoractivo.nextSibling){
-        indicadoractivo.nextElementSibling.classList.add('activo');
-        indicadoractivo.classList.remove('activo');
-    }
-
-    juegos2.forEach(juego => juego.classList.remove('hover'));
- 
-    setTimeout(() => {
-        flechaderecha2.classList.remove('flecha-oculta');
-    }, 500);
-});
-
-const numeroPaginas2 = Math.ceil(juegos2.length / 5);
-
-
-for (let i = 0; i < numeroPaginas2; i++) {
-    const indicator1 = document.createElement('button');
-
-    if (i == 0) {
-        indicator1.classList.add('activo');
-    }
-
-    document.getElementById('indicadores-2').appendChild(indicator1);
-
-    indicator1.addEventListener('click', (e) => {
-        fila2.scrollLeft = i * fila2.offsetWidth;
-
-        document.getElementById('indicadores-2').querySelector('.indicadores .activo').classList.remove('activo');
-        e.target.classList.add('activo');
-    });
+function generarFlechas() {
+    generarFlecha(1);
+    generarFlecha(2);
 }
 
-juegos2.forEach((juego) => {
-    juego.addEventListener('mouseenter', (e) => {
-        const elemento = e.currentTarget;
-        setTimeout(() => {
-            juegos2.forEach(juego => juego.classList.remove('hover'));
-            elemento.classList.add('hover');
-            
-            let imagen = elemento.querySelector('img');
-            if(!imagen.getAttribute('src').includes('_hover')) {
-                //Solo para desktop
-                //Reemplazamos la img sin hover por la con hover
-                let urlImgHover = imagen.getAttribute('src').replace('.','_hover.');
-                imagen.setAttribute('src',urlImgHover);
-            }
-        }, 200);
-    });
+function renderIndicador(id, imagenesXpagina) {
+    var juegos = document.getElementById('carrusel-' + id).querySelectorAll('.juego');
+    var numeroPag = Math.ceil(juegos.length / imagenesXpagina);
 
-    juego.addEventListener('mouseleave', (e) => {
-        const elemento = e.currentTarget;
-        setTimeout(() => {
-            //Removemos el hover cuando el mouse sale de la imagen, cambia la tarjeta 
-            elemento.classList.remove('hover');
-            
-            let imagen = juego.querySelector('img');
-            let urlImg = imagen.getAttribute('src').replaceAll('_hover','');
-            imagen.setAttribute('src', urlImg);
-        }, 200);
-    });
-});
+    var div = document.getElementById('indicadores-' + id);
+    var fila = document.getElementById('contenedor-carrusel-' + id);
+    div.innerHTML = '';
 
-
-
-
-const mediaQuery1 = window.matchMedia("(max-width: 450px)");
-const mediaQuery4 = window.matchMedia("(min-width: 451px)");
-
-function handleTabletChange1(e) {
-  if (e.matches) {
-    renderMobile();
-  }
-}
-  
-function handleTabletChange4(e) {
-    if (e.matches) {
-        renderDesktop();
+    for (let i = 0; i < numeroPag; i++) {
+        const indicator = document.createElement('button');
+    
+        if (i == 0) {
+            indicator.classList.add('activo');
+        }
+        
+        div.appendChild(indicator);  
+        indicator.addEventListener('click', (e) => {
+            fila.scrollLeft = i * fila.offsetWidth;  
+            div.querySelector('.indicadores .activo').classList.remove('activo');
+            e.target.classList.add('activo');
+        });
     }
 }
 
-// Register event listener
-mediaQuery1.addListener(handleTabletChange1);
-mediaQuery4.addListener(handleTabletChange4);
+function renderIndicadores (imagenesXpagina){
+    renderIndicador(1, imagenesXpagina);
+    renderIndicador(2, imagenesXpagina);
+}
+
+function renderHover (id) {
+    var juegos = document.getElementById('carrusel-' + id).querySelectorAll('.juego');
+
+    juegos.forEach((juego) => {
+        juego.addEventListener('mouseenter', (e) => {
+            var elemento = e.currentTarget;
+            setTimeout(() => {
+                juegos.forEach(juego => juego.classList.remove('hover'));
+                elemento.classList.add('hover');
+    
+                let imagen = elemento.querySelector('img');
+                if(!imagen.getAttribute('src').includes('_hover')) {
+                    //Solo para desktop
+                    //Reemplazamos la img sin hover por la con hover
+                    let urlImgHover = imagen.getAttribute('src').replace('.','_hover.');
+                    imagen.setAttribute('src',urlImgHover);
+                }
+            }, 200);
+        });
+    
+        juego.addEventListener('mouseleave', (e) => {
+            var elemento = e.currentTarget;
+            setTimeout(() => {
+                elemento.classList.remove('hover');
+                let imagen = elemento.querySelector('img');
+                let urlImg = imagen.getAttribute('src').replaceAll('_hover','');
+                imagen.setAttribute('src', urlImg);
+            }, 200);
+        });
+    });
+}
+
+function renderHovers() {
+    renderHover(1);
+    renderHover(2);
+}
 
 function renderDesktop() {
     let sectionCarruseles = document.querySelector('.section-carruseles');
@@ -266,7 +174,7 @@ function renderDesktop() {
     });
 }
 
-function renderMobile(){
+function renderMobile() {
     let sectionCarruseles = document.querySelector('.section-carruseles');
     let imagenes = sectionCarruseles.querySelectorAll('img');
 
@@ -277,6 +185,34 @@ function renderMobile(){
         }
     });
 }
+
+const mediaQuery1 = window.matchMedia("(max-width: 450px)");
+const mediaQuery4 = window.matchMedia("(min-width: 451px)");
+
+function handleTabletChange1(e) {
+    if (e.matches) {
+        renderIndicadores(2);
+        renderMobile();
+    }
+}
+  
+function handleTabletChange4(e) {
+    if (e.matches) {
+        renderIndicadores(5);
+        renderDesktop();
+    }
+}
+
+renderIndicadores(5);
+generarFlechas();
+renderHovers();
+
+// Register event listener
+mediaQuery1.addListener(handleTabletChange1);
+mediaQuery4.addListener(handleTabletChange4);
+
+
+
 
 let footer = document.getElementById('fat_footer');
 console.log(footer);
