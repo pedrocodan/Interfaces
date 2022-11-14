5b9fbe0e70e438cc801a56538e8a7d47aad57ad6
Pedro Codan <codanpedro@gmail.com>
2022-11-14 18:14:35 -0300

    humo1

--- Entregable4/js/animacionLanzamiento.js
+++ Entregable4/js/animacionLanzamiento.js
@@ -19,7 +19,18 @@ function reveal() {
   window.addEventListener("scroll", reveal);
 
   window.addEventListener("scroll", function() {
-    let distance = window.scrollY
-    document.querySelector(".lanzamiento-inicio-pubg").style.transform = `translateY(${distance *0.66}px)`
-    document.querySelector(".lanzamiento-pubg-logo").style.transform = `translateY(${distance * (-0.3)}px)`
+
+    let distance = window.scrollY;
+    let opacidadActual = document.querySelector(".lanzamiento-inicio-pubg").style.opacity
+    document.querySelector(".lanzamiento-inicio-pubg").style.opacity =1
+    if(distance>0){
+        document.querySelector(".lanzamiento-inicio-pubg").style.transform = `translateY(${distance *0.66}px)`;
+        document.querySelector(".lanzamiento-pubg-logo").style.transform = `translateY(${distance * (-0.3)}px)`;
+        document.querySelector(".lanzamiento-main-div").style.transform = `translateY(0px)`;
+        document.querySelector(".lanzamiento-inicio-pubg").style.opacity = opacidadActual*0.95;   
+    }
+    if (distance>800){
+        document.querySelector(".lanzamiento-inicio-pubg").style.opacity = 1;
+    }
   })
+
